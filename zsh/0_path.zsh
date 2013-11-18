42b4bb4df1c11acb47acc37e8594886e6c5f3fc9
Adam Riggall <adam@riggall.info>
2013-11-18 12:02:03 -0600

    Add afni to path if present

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,2 +1,7 @@
 # path, the 0 in the filename causes this to load first
 export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
+
+if [[ -d /usr/local/afni ]]
+then
+  export PATH=$PATH:/usr/local/afni
+fi
