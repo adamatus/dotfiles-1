7b7b5a652087ba4691cb185faec5d87514f20b98
Adam Riggall <adam@riggall.info>
2013-11-18 23:37:09 -0600

    Update paths

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -4,4 +4,5 @@ export PATH=$PATH:$HOME/.yadr/bin:$HOME/.yadr/bin/yadr
 if [[ -d /usr/local/afni ]]
 then
   export PATH=$PATH:/usr/local/afni
+  export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/afni
 fi
